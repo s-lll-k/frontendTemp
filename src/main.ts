eae72d563b199f8b9fd79378abcb066e38175d9a
s-lll-k <sylik3210@gmail.com>
2024-12-08 23:39:09 +0500

    НЕ ДОДЕЛАНО!!!

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import './assets/styles/main.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
---
+++ src/pages/TemplatePage.vue
@@ -0,0 +1,4 @@
+
+<template>
+    <HelloWorld msg="Vite + Vue + TS" />
+</template>
